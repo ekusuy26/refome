7ae489e1b8f91af328ddeac1ff5decc00390b74e
ekusuy26 <y.yuu6221@gmail.com>
2020-05-20 01:14:22 +0900

    New post routing settings

--- app/Food.php
+++ app/Food.php
@@ -6,5 +6,6 @@ use Illuminate\Database\Eloquent\Model;
 
 class Food extends Model
 {
-    //
+    // データの追加や更新が可能
+    protected $fillable = ['user_id', 'name', 'quantity'];
 }
--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,5 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::get('/foods/new', 'Auth\FoodController@index')->name('foods.new');
