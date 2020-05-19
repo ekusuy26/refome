1ea16367fe394741725cd00b415a7817fc7f51d9
ekusuy26 <y.yuu6221@gmail.com>
2020-05-20 00:35:08 +0900

    Edit the introduction text on the login screen

--- resources/views/top.blade.php
+++ resources/views/top.blade.php
@@ -5,8 +5,8 @@
 @if(!Auth::check())
 <div id="login-wrapper" class="row">
     <div class="col-7">
-        <h1 class="text-white"><b>Hello Hackers！</b></h1>
-        <p class="text-white">Qiita風は、テックピットマーケットより発売されている、Qiita風記事投稿サービスの教材です。<br>Qitaっぽいサイトを作ってスキルアップしましょう！ ;)<br>コードを書いていて気づいたこと・学んだことはバンバンシェアしてください！</p>
+        <h1 class="text-white"><b>冷蔵庫に何があったっけ！？</b></h1>
+        <p class="text-white">refomeはあなたの冷蔵庫に何がストックされてるかを管理出来ます。<br>賢く食材を使い切りましょう！<br>作った料理のレシピのシェアも可能です。</p>
     </div>
     <div class="col-5">
         <form method="POST" action="{{ route('login') }}">
@@ -14,15 +14,15 @@
             <table>
                 <tr>
                     <th>ユーザ名</th>
-                    <td><input type="text" class="form-control" placeholder="qiita風たん" size="50" value="{{ old('email') }}" name="username" required autofocus></td>
+                    <td><input type="text" class="form-control" placeholder="username" size="50" value="{{ old('email') }}" name="username" required autofocus></td>
                 </tr>
                 <tr>
                     <th>メールアドレス</th>
-                    <td><input type="email" class="form-control" placeholder="qiitahuu@qiitahuu.com" size="50" name="email" required></td>
+                    <td><input type="email" class="form-control" placeholder="email" size="50" name="email" required></td>
                 </tr>
                 <tr>
                     <th>パスワード</th>
-                    <td><input type="password" class="form-control" name="password" required size="50"></td>
+                    <td><input type="password" class="form-control" placeholder="password"  name="password" required size="50"></td>
                 </tr>
                 <tr>
                     <th></th>
