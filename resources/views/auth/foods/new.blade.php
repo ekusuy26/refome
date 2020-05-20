1f13184a92c205c493335d9416a9ce7ef010eb4b
ekusuy26 <y.yuu6221@gmail.com>
2020-05-20 11:00:09 +0900

    Create a food posting form

--- resources/views/auth/foods/new.blade.php
+++ resources/views/auth/foods/new.blade.php
@@ -1,1 +1,12 @@
-hogehoge
+@extends('layouts.common')
+@section('content')
+<form class="post-page-wrapper" action="/drafts/new" method="post">
+@csrf
+    <input type="text" class="form-control m-1" id="title-input" placeholder="食材" name="name">
+    <input type="text" class="form-control m-1" placeholder="数量" name="quantity">
+    <div class="post-page-footer">
+        <input type="submit" class="post-button m-1" value="冷蔵庫に入れる">
+    </div>
+</form>
+
+@endsection
