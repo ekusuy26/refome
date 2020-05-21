ed7e9299f325d5b80d513437d528def525cb4677
ekusuy26 <y.yuu6221@gmail.com>
2020-05-21 10:22:58 +0900

    Set the send form in the view of recipe posting

--- resources/views/auth/posts/new.blade.php
+++ resources/views/auth/posts/new.blade.php
@@ -1,1 +1,14 @@
-hogehoge
+@extends('layouts.common')
+@section('content')
+<form class="post-page-wrapper" action="/posts/new" method="post">
+@csrf
+    <input type="text" class="form-control m-1" id="title-input" placeholder="タイトル" name="title">
+    <input type="file" class="form-control m-1" placeholder="写真を選択" name="image">
+    <input type="text" class="form-control m-1" placeholder="材料" name="material">
+    <input type="text" class="form-control m-1" placeholder="説明文" name="body">
+    <div class="post-page-footer">
+        <input type="submit" class="post-button m-1" value="レシピを投稿">
+    </div>
+</form>
+
+@endsection
