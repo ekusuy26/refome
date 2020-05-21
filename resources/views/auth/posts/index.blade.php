8373be43b7db1f2620e9f5c6240e077746ba2109
ekusuy26 <y.yuu6221@gmail.com>
2020-05-21 11:30:47 +0900

    Create class in html

--- resources/views/auth/posts/index.blade.php
+++ resources/views/auth/posts/index.blade.php
@@ -1,11 +1,21 @@
 @extends('layouts.common')
 @section('content')
 
-@foreach ($articles as $article)
-{{$article->title}}
-{{$article->image}}
-{{$article->user_id}}
-{{$article->created_at}}
-@endforeach
+<div class="posts-wrapper">
+  <div class="posts-wrapper col-md-6">
+    @foreach ($articles as $article)
+    <div class="article-box">
+      <div class="article-box-left">{{$article->image}}</div>
+      <div class="article-box-right">
+        <a class="article-title" href="#">{{ $article->title }}</a>
+        <div class="article-details">
+          <div class="article-data">{{ $article->user_id }}</div>
+          <div class="article-data">{{ $article->created_at }}</div>
+        </div>
+      </div>
+    </div>
+    @endforeach
+  </div>
+</div>
 
 @endsection
