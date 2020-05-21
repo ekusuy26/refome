44358a8eb3786996491e4faa70c575f2474b3717
ekusuy26 <y.yuu6221@gmail.com>
2020-05-21 11:26:42 +0900

    View all retrieved recipe data in a view

--- resources/views/auth/posts/index.blade.php
+++ resources/views/auth/posts/index.blade.php
@@ -1,6 +1,11 @@
 @extends('layouts.common')
 @section('content')
 
-
+@foreach ($articles as $article)
+{{$article->title}}
+{{$article->image}}
+{{$article->user_id}}
+{{$article->created_at}}
+@endforeach
 
 @endsection
