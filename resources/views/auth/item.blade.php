76efac34d46ff578906acdae6f8011c0c5b8ea39
ekusuy26 <y.yuu6221@gmail.com>
2020-05-21 10:54:21 +0900

    View database information in the recipe details view

--- resources/views/auth/item.blade.php
+++ resources/views/auth/item.blade.php
@@ -1,4 +1,16 @@
 @extends('layouts.common')
 @section('content')
 
+<div class="item-page-wrapper">
+  <div class="item-wrapper">
+    <div class="item-header">
+      <div class="date">{{$article->created_at}}</div>
+    </div>
+    <div class="item-title">{{$article->title}}</div>
+    <div class="item-image">{{$article->image}}</div>
+    <div class="item-material">{{$article->material}}</div>
+    <div class="item-body">{{$article->body}}</div>
+  </div>
+</div>
+
 @endsection
