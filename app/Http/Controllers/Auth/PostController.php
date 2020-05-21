b375c828b9f0115e6eb7aa62ef245329041ecb32
ekusuy26 <y.yuu6221@gmail.com>
2020-05-21 10:33:57 +0900

    Implementation of recipe posting function

--- app/Http/Controllers/Auth/PostController.php
+++ app/Http/Controllers/Auth/PostController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Post;
+use Illuminate\Support\Facades\Auth;
 
 class PostController extends Controller
 {
@@ -11,4 +13,25 @@ class PostController extends Controller
     {
         return view('auth.posts.new');
     }
+
+    public function postArticle(Request $request)
+    {
+        // バリデーション
+        $request->validate([
+            'title' => 'required|unique:posts|max:255',
+            'image' => 'required|string',
+            'material' => 'required|integer',
+            'body' => 'required|string',
+        ]);
+
+        Post::create([
+            'user_id' => Auth::user()->id,
+            'title' => $request->title,
+            'image' => $request->image,
+            'material' => $request->material,
+            'body' => $request->body,
+        ]);
+        return redirect('/');
+    }
+
 }
