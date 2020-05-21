177d12828c5bb62e5f62ca6f975542eb97989b42
ekusuy26 <y.yuu6221@gmail.com>
2020-05-21 15:34:35 +0900

    Adjust image size

--- resources/views/auth/posts/index.blade.php
+++ resources/views/auth/posts/index.blade.php
@@ -2,11 +2,11 @@
 @section('content')
 
 <div class="posts-wrapper">
-  <div class="posts-wrapper col-md-6">
+  <div class="post-wrapper col-md-6">
     @foreach ($articles as $article)
     <div class="article-box">
       <div class="article-box-left">
-        <img src="{{ asset('/storage/img/'.$article->image) }}">
+        <img src="{{ asset('/storage/img/'.$article->image) }}" style="height: 150px; width: 150px">
       </div>
       <div class="article-box-right">
         <a class="article-title" href="/posts/{{$article->id}}">{{ $article->title }}</a>
