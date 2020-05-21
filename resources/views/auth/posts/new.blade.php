b89cbd20ef9dfb7d9ef9b7df7216c600812f1a82
ekusuy26 <y.yuu6221@gmail.com>
2020-05-21 16:29:43 +0900

    Layout correction of recipe posting screen html

--- resources/views/auth/posts/new.blade.php
+++ resources/views/auth/posts/new.blade.php
@@ -1,14 +1,29 @@
 @extends('layouts.common')
 @section('content')
-<form class="post-page-wrapper" action="/posts/new" method="post" enctype="multipart/form-data">
-@csrf
-    <input type="text" class="form-control m-1" id="title-input" placeholder="タイトル" name="title">
-    <input type="file" class="form-control m-1" placeholder="写真を選択" name="image">
-    <input type="text" class="form-control m-1" placeholder="材料" name="material">
-    <input type="text" class="form-control m-1" placeholder="説明文" name="body">
-    <div class="post-page-footer">
-        <input type="submit" class="post-button m-1" value="レシピを投稿">
-    </div>
-</form>
+
+<div class="post-new-wrapper">
+    <form class="post-page-wrapper" action="/posts/new" method="post" enctype="multipart/form-data">
+        @csrf
+        <div class="post-new-box mx-auto" style="width: 80%;">
+            <div class="post-new-box-left">
+                <input type="file" class="form-control" placeholder="写真を選択" name="image">
+            </div>
+            <div class="post-new-box-right">
+                <div class="post-new-title">
+                    <input type="text" class="form-control" id="title-input" placeholder="タイトル" name="title">
+                </div>
+                <div class="post-new-detail">
+                    <input type="text" class="form-control" placeholder="説明文" name="body">
+                    <div class="post-new-material">
+                        <input type="text" class="form-control" placeholder="材料" name="material">
+                        <div class="post-page-footer">
+                            <input type="submit" class="post-button" value="レシピを投稿">
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </form>
+</div>
 
 @endsection
