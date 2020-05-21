2b765992baf195b1a1f4425de8be3d7c15ab9763
ekusuy26 <y.yuu6221@gmail.com>
2020-05-21 23:31:52 +0900

    Fix layout

--- resources/views/auth/foods/delete.blade.php
+++ resources/views/auth/foods/delete.blade.php
@@ -2,14 +2,20 @@
 @section('content')
 <form class="post-page-wrapper" action="/foods/delete" method="post">
   @csrf
-  @if($errors->first('name'))
-  <div class="validation">{{ $errors->first('name') }}</div>
-  @endif
-  <input type="text" class="form-control mx-auto m-1" style="width:50%;" id="title-input" placeholder="食材" name="name">
-  @if($errors->first('quantity'))
-  <div class="validation">{{ $errors->first('quantity') }}</div>
-  @endif
-  <input type="text" class="form-control mx-auto m-1" style="width:50%;" placeholder="数量" name="quantity">
+  <div class="food-wrap mx-auto m-1" style="width:50%">
+    <div class="food-box col-md-6">
+      @if($errors->first('name'))
+      <div class="validation m-1">{{ $errors->first('name') }}</div>
+      @endif
+      <input type="text" class="form-control m-1" id="title-input" placeholder="食材" name="name">
+    </div>
+    <div class="food-box col-md-6">
+      @if($errors->first('quantity'))
+      <div class="validation m-1">{{ $errors->first('quantity') }}</div>
+      @endif
+      <input type="text" class="form-control m-1" placeholder="数量" name="quantity">
+    </div>
+  </div>
   <div class="post-page-footer mx-auto m-1" style="width:50%;">
     <input type="submit" class="post-button m-1" value="冷蔵庫から取り出す">
   </div>
