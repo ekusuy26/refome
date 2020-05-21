cf1688d6856581b6d4ef4f692e344862e01b27a1
ekusuy26 <y.yuu6221@gmail.com>
2020-05-21 19:08:24 +0900

    Fixed scss for individual display of recipes

--- resources/views/auth/posts/show.blade.php
+++ resources/views/auth/posts/show.blade.php
@@ -2,15 +2,15 @@
 @section('content')
 
 <div class="post-show-wrapper">
-  <div class="post-show-box mx-auto" style="width: 80%;">
+  <div class="post-show-box mx-auto">
     <div class="post-show-box-left">
-      <img src="{{ asset('/storage/img/'.$article->image) }}" style="height: 300px; width: 300px">
+      <img src="{{ asset('/storage/img/'.$article->image) }}" style="width: 100%">
     </div>
     <div class="post-show-box-right">
       <div class="post-show-title">TITLE：{{$article->title}}</div>
       <div class="post-show-detail">
         <div class="post-show-body">introduction：{{$article->body}}</div>
-        <div class="post-show-material">material：{{$article->material}}</div>
+        <div class="post-show-body">material：{{$article->material}}</div>
       </div>
     </div>
   </div>
