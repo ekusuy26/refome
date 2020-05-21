fd6d8988f7d150d018390fd417c9ea92bf352979
ekusuy26 <y.yuu6221@gmail.com>
2020-05-21 17:15:41 +0900

    Fixed html of individual display of recipe

--- resources/views/auth/posts/show.blade.php
+++ resources/views/auth/posts/show.blade.php
@@ -1,15 +1,18 @@
 @extends('layouts.common')
 @section('content')
 
-<div class="item-page-wrapper">
-  <div class="item-wrapper">
-    <div class="item-header">
-      <div class="date">{{$article->created_at}}</div>
+<div class="post-show-wrapper">
+  <div class="post-show-box mx-auto" style="width: 80%;">
+    <div class="post-show-box-left">
+      <img src="{{ asset('/storage/img/'.$article->image) }}" style="height: 300px; width: 300px">
     </div>
-    <div class="item-title">{{$article->title}}</div>
-    <div class="item-image">{{$article->image}}</div>
-    <div class="item-material">{{$article->material}}</div>
-    <div class="item-body">{{$article->body}}</div>
+    <div class="post-show-box-right">
+      <div class="post-show-title">TITLE：{{$article->title}}</div>
+      <div class="post-show-detail">
+        <div class="post-show-body">introduction：{{$article->body}}</div>
+        <div class="post-show-material">material：{{$article->material}}</div>
+      </div>
+    </div>
   </div>
 </div>
 
