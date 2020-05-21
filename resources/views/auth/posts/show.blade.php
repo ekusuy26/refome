7302d94c14d9dc0ffad4c721270c9dc8f88ddebd
ekusuy26 <y.yuu6221@gmail.com>
2020-05-21 22:52:14 +0900

    Display title in each box

--- resources/views/auth/posts/show.blade.php
+++ resources/views/auth/posts/show.blade.php
@@ -7,10 +7,16 @@
       <img src="{{ asset('/storage/img/'.$article->image) }}" style="width: 100%">
     </div>
     <div class="post-show-box-right">
-      <div class="post-show-title">TITLE：{{$article->title}}</div>
+      <div class="post-show-title">{{$article->title}}</div>
       <div class="post-show-detail">
-        <div class="post-show-body">introduction：{{$article->body}}</div>
-        <div class="post-show-body">material：{{$article->material}}</div>
+        <div class="post-show-body">
+        <div class="post-show-body-text"><span class="under">　手順　</span></div>
+        {{$article->body}}
+        </div>
+        <div class="post-show-body">
+        <div class="post-show-body-text"><span class="under">　材料　</span></div>
+        {{$article->material}}
+        </div>
       </div>
     </div>
   </div>
