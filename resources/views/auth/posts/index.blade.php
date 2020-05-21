97c714cf05ae7010278beeca48480a31e21cc50c
ekusuy26 <y.yuu6221@gmail.com>
2020-05-21 11:33:27 +0900

    Set a link from the list to details

--- resources/views/auth/posts/index.blade.php
+++ resources/views/auth/posts/index.blade.php
@@ -7,7 +7,7 @@
     <div class="article-box">
       <div class="article-box-left">{{$article->image}}</div>
       <div class="article-box-right">
-        <a class="article-title" href="#">{{ $article->title }}</a>
+        <a class="article-title" href="/posts/{{$article->id}}">{{ $article->title }}</a>
         <div class="article-details">
           <div class="article-data">{{ $article->user_id }}</div>
           <div class="article-data">{{ $article->created_at }}</div>
