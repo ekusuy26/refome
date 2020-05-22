1ab229c5b8eb8447b0c991bc3604098786f0090c
ekusuy26 <y.yuu6221@gmail.com>
2020-05-22 09:12:28 +0900

    Added to post model to show likes

--- app/Post.php
+++ app/Post.php
@@ -7,4 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 class Post extends Model
 {
     protected $fillable = ['title', 'image', 'material', 'body', 'user_id'];
+
+    public function favorite_users()
+    {
+            return $this->belongsToMany(User::class,'favorites','post_id','user_id')->withTimestamps();
+    }
 }
