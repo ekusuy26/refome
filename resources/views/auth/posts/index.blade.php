2f9b006d6da6ed67b84cb188a1dad32082dc14ab
ekusuy26 <y.yuu6221@gmail.com>
2020-05-22 11:45:42 +0900

    Change the link to individual pages to the entire box

--- resources/views/auth/posts/index.blade.php
+++ resources/views/auth/posts/index.blade.php
@@ -4,21 +4,23 @@
 <div class="posts-wrapper">
   <div class="post-wrapper col-md-6">
     @foreach ($articles as $article)
-    <div class="article-box">
-      <div class="article-box-left">
-        <img src="{{ asset('/storage/img/'.$article->image) }}" style="height: 150px; width: 150px">
-      </div>
-      <div class="article-box-right">
-        <a class="article-title" href="/posts/{{$article->id}}">【料理名】{{ $article->title }}</a>
-        <div class="article-details">
-          <div class="article-data">【作成者】{{ $article->user->name }}</div>
-          <div class="article-data">【作成日】{{ $article->created_at->format('Y/m/d') }}</div>
+    <a href="/posts/{{$article->id}}" style="text-decoration:none;">
+      <div class="article-box">
+        <div class="article-box-left">
+          <img src="{{ asset('/storage/img/'.$article->image) }}" style="height: 150px; width: 150px">
         </div>
-        <div class="article-like text-right mb-2">いいね！
-          <span class="badge badge-pill badge-success">{{ $article->favorite_users()->count() }}</span>
+        <div class="article-box-right">
+          <div class="article-title">【料理名】{{ $article->title }}</div>
+          <div class="article-details">
+            <div class="article-data">【作成者】{{ $article->user->name }}</div>
+            <div class="article-data">【作成日】{{ $article->created_at->format('Y/m/d') }}</div>
+          </div>
+          <div class="article-like text-right mb-2">いいね！
+            <span class="badge badge-pill badge-success">{{ $article->favorite_users()->count() }}</span>
+          </div>
         </div>
       </div>
-    </div>
+    </a>
     @endforeach
   </div>
 </div>
