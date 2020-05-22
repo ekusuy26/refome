671a7891f04989471c45bc4100e66e637975b671
ekusuy26 <y.yuu6221@gmail.com>
2020-05-22 09:31:30 +0900

    create iine btn

--- resources/views/auth/posts/show.blade.php
+++ resources/views/auth/posts/show.blade.php
@@ -5,6 +5,23 @@
   <div class="post-show-box mx-auto">
     <div class="post-show-box-left">
       <img src="{{ asset('/storage/img/'.$article->image) }}" style="width: 100%">
+      @if (Auth::id() != $user->id)
+      
+          @if (Auth::user()->is_favorite($post->id))
+      
+              {!! Form::open(['route' => ['favorites.unfavorite', $post->id], 'method' => 'delete']) !!}
+                  {!! Form::submit('いいね！を外す', ['class' => "button btn btn-warning"]) !!}
+              {!! Form::close() !!}
+      
+          @else
+      
+              {!! Form::open(['route' => ['favorites.favorite', $post->id]]) !!}
+                  {!! Form::submit('いいね！を付ける', ['class' => "button btn btn-success"]) !!}
+              {!! Form::close() !!}
+      
+          @endif
+      
+      @endif
     </div>
     <div class="post-show-box-right">
       <div class="post-show-title">{{$article->title}}</div>
