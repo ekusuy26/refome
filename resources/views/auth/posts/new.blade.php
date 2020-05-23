a4b00a0f684ca7f59684ae827a0c2d567aae2aad
ekusuy26 <y.yuu6221@gmail.com>
2020-05-24 00:53:38 +0900

    fix layout

--- resources/views/auth/posts/new.blade.php
+++ resources/views/auth/posts/new.blade.php
@@ -6,7 +6,7 @@
         @csrf
         <div class="post-new-box mx-auto" style="width: 80%;">
             <div class="post-new-box-left">
-                <input type="file" class="form-control" id="img-file" name="image">
+                <input type="file" class="form-control-file" id="img-file" name="image">
                 <div class="img-preview" id="img-preview">
                     <img id="thumbnail" class="img-preview_content" accept="image/*" src="">
                 </div>
@@ -18,8 +18,10 @@
                 <div class="post-new-detail">
                     <textarea cols="60" class="form-control" id="body-input" placeholder="説明文" name="body"></textarea>
                     <div class="post-new-material">
-                        <input type="text" class="form-control" placeholder="材料" name="name">
-                        <input type="text" class="form-control" placeholder="数量" name="quantity">
+                        <div class="post-new-material-box">
+                            <input type="text" class="form-control" placeholder="材料" name="name">
+                            <input type="text" class="form-control" placeholder="数量" name="quantity">
+                        </div>
                         <div class="post-page-footer">
                             <input type="submit" class="post-button" value="レシピを投稿">
                         </div>
