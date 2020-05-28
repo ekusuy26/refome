72128917f6936daa735ad72a4da2591ebd458f87
ekusuy26 <y.yuu6221@gmail.com>
2020-05-28 14:40:07 +0900

    Create edit screen layout

--- app/Http/Controllers/Auth/PostController.php
+++ app/Http/Controllers/Auth/PostController.php
@@ -103,7 +103,7 @@ class PostController extends Controller
 
     public function edit($id)
     {
-        $article = Post::where('id', $id);
+        $article = Post::where('id', $id)->get();
         return view('auth.posts.edit', compact('article'));
     }
 }
--- resources/views/auth/posts/edit.blade.php
+++ resources/views/auth/posts/edit.blade.php
@@ -1,1 +1,44 @@
-hogehoge
+@extends('layouts.common')
+@section('content')
+
+<div class="post-new-wrapper">
+    <form class="post-page-wrapper" action="/posts/new" method="post" enctype="multipart/form-data">
+        @csrf
+        <div class="post-new-box mx-auto" style="width: 80%;">
+            <div class="post-new-box-left">
+                <input type="file" class="form-control-file" id="img-file" name="image">
+                <div class="img-preview" id="img-preview">
+                    <img id="thumbnail" class="img-preview_content" accept="image/*" src="">
+                </div>
+            </div>
+            <div class="post-new-box-right">
+                    {{ $article->title }}
+                <div class="post-new-title">
+                    <input type="text" class="form-control" id="title-input" placeholder="タイトル" name="title">
+                </div>
+                <div class="post-new-detail">
+                    <textarea cols="60" class="form-control" id="body-input" placeholder="説明文" name="body"></textarea>
+                    <div class="post-new-material">
+                        <div class="post-new-material-box">
+                            <input type="text" class="form-control" placeholder="材料" name="name1">
+                            <input type="text" class="form-control" placeholder="数量" name="quantity1">
+                        </div>
+                        <div class="post-new-material-box">
+                            <input type="text" class="form-control" placeholder="材料" name="name2">
+                            <input type="text" class="form-control" placeholder="数量" name="quantity2">
+                        </div>
+                        <div class="post-new-material-box">
+                            <input type="text" class="form-control" placeholder="材料" name="name3">
+                            <input type="text" class="form-control" placeholder="数量" name="quantity3">
+                        </div>
+                        <div class="post-page-footer">
+                            <input type="submit" class="post-button btn-lg" value="レシピを投稿">
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </form>
+</div>
+
+@endsection
