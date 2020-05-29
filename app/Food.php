5148262ce47067f66e35e1a28bcfdc79e2698572
ekusuy26 <y.yuu6221@gmail.com>
2020-05-29 16:39:34 +0900

    Describe the relationship between post and food in the food model

--- app/Food.php
+++ app/Food.php
@@ -9,4 +9,9 @@ class Food extends Model
     // データの追加や更新が可能
     protected $fillable = ['user_id', 'post_id', 'name', 'quantity'];
     protected $table = 'foods';
+
+    public function post()
+      {
+        return $this->belongsTo(Post::class);
+      }
 }
