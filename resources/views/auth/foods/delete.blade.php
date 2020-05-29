41037ef4488283f139dce633acd56c959bb31932
ekusuy26 <y.yuu6221@gmail.com>
2020-05-29 18:40:51 +0900

    Numbering input field

--- resources/views/auth/foods/delete.blade.php
+++ resources/views/auth/foods/delete.blade.php
@@ -9,29 +9,29 @@
         @if($errors->first('name1'))
         <div class="validation m-1">{{ $errors->first('name1') }}</div>
         @endif
-        <input type="text" class="form-control m-1" id="title-input" placeholder="食材" name="name1">
+        <input type="text" class="form-control m-1" id="title-input" placeholder="食材1" name="name1">
       </div>
       <div class="food-box col-md-6">
         @if($errors->first('quantity1'))
         <div class="validation m-1">{{ $errors->first('quantity1') }}</div>
         @endif
-        <input type="text" class="form-control m-1" placeholder="数量" name="quantity1">
+        <input type="text" class="form-control m-1" placeholder="数量1" name="quantity1">
       </div>
     </div>
     <div class="food-wrap mx-auto m-1" style="width:50%">
       <div class="food-box col-md-6">
-        <input type="text" class="form-control m-1" id="title-input" placeholder="食材" name="name2">
+        <input type="text" class="form-control m-1" id="title-input" placeholder="食材2" name="name2">
       </div>
       <div class="food-box col-md-6">
-        <input type="text" class="form-control m-1" placeholder="数量" name="quantity2">
+        <input type="text" class="form-control m-1" placeholder="数量2" name="quantity2">
       </div>
     </div>
     <div class="food-wrap mx-auto m-1" style="width:50%">
       <div class="food-box col-md-6">
-        <input type="text" class="form-control m-1" id="title-input" placeholder="食材" name="name3">
+        <input type="text" class="form-control m-1" id="title-input" placeholder="食材3" name="name3">
       </div>
       <div class="food-box col-md-6">
-        <input type="text" class="form-control m-1" placeholder="数量" name="quantity3">
+        <input type="text" class="form-control m-1" placeholder="数量3" name="quantity3">
       </div>
     </div>
     <div class="post-page-footer mx-auto m-1" style="width:50%;">
