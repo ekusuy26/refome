9df4fc55d6d26bef2bfce8922ae7fb36e1b62656
ekusuy26 <y.yuu6221@gmail.com>
2020-05-29 18:39:14 +0900

    In the same way as for taking out, an error message is displayed and unnecessary words are deleted.

--- resources/views/auth/foods/delete.blade.php
+++ resources/views/auth/foods/delete.blade.php
@@ -6,43 +6,31 @@
     @csrf
     <div class="food-wrap mx-auto m-1" style="width:50%">
       <div class="food-box col-md-6">
-        @if($errors->first('name'))
-        <div class="validation m-1">{{ $errors->first('name') }}</div>
+        @if($errors->first('name1'))
+        <div class="validation m-1">{{ $errors->first('name1') }}</div>
         @endif
         <input type="text" class="form-control m-1" id="title-input" placeholder="食材" name="name1">
       </div>
       <div class="food-box col-md-6">
-        @if($errors->first('quantity'))
-        <div class="validation m-1">{{ $errors->first('quantity') }}</div>
+        @if($errors->first('quantity1'))
+        <div class="validation m-1">{{ $errors->first('quantity1') }}</div>
         @endif
         <input type="text" class="form-control m-1" placeholder="数量" name="quantity1">
       </div>
     </div>
     <div class="food-wrap mx-auto m-1" style="width:50%">
       <div class="food-box col-md-6">
-        @if($errors->first('name'))
-        <div class="validation m-1">{{ $errors->first('name') }}</div>
-        @endif
         <input type="text" class="form-control m-1" id="title-input" placeholder="食材" name="name2">
       </div>
       <div class="food-box col-md-6">
-        @if($errors->first('quantity'))
-        <div class="validation m-1">{{ $errors->first('quantity') }}</div>
-        @endif
         <input type="text" class="form-control m-1" placeholder="数量" name="quantity2">
       </div>
     </div>
     <div class="food-wrap mx-auto m-1" style="width:50%">
       <div class="food-box col-md-6">
-        @if($errors->first('name'))
-        <div class="validation m-1">{{ $errors->first('name') }}</div>
-        @endif
         <input type="text" class="form-control m-1" id="title-input" placeholder="食材" name="name3">
       </div>
       <div class="food-box col-md-6">
-        @if($errors->first('quantity'))
-        <div class="validation m-1">{{ $errors->first('quantity') }}</div>
-        @endif
         <input type="text" class="form-control m-1" placeholder="数量" name="quantity3">
       </div>
     </div>
