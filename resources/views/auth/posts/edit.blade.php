f0e9ec0eeef5407948ce602bbb49f9edde30212b
ekusuy26 <y.yuu6221@gmail.com>
2020-05-29 16:36:13 +0900

    Display image on edit screen

--- resources/views/auth/posts/edit.blade.php
+++ resources/views/auth/posts/edit.blade.php
@@ -6,9 +6,9 @@
         @csrf
         <div class="post-new-box mx-auto" style="width: 80%;">
             <div class="post-new-box-left">
-                <input type="file" class="form-control-file" id="img-file" name="image" value="{{$article->image}}">
+                <input type="file" class="form-control-file" id="img-file" name="image">
                 <div class="img-preview" id="img-preview">
-                    <img id="thumbnail" class="img-preview_content" accept="image/*" src="">
+                    <img id="thumbnail" class="img-preview_content" accept="image/*" src="https://refome20200527.s3-ap-northeast-1.amazonaws.com/myprefix/{{$article->image}}" style="height:300px; width:300px;">
                 </div>
             </div>
             <div class="post-new-box-right">
