55227d3fa043303b0b2e5d0cefaa41a152f89f03
ekusuy26 <y.yuu6221@gmail.com>
2020-05-29 18:37:44 +0900

    Delete unnecessary words

--- resources/views/auth/foods/new.blade.php
+++ resources/views/auth/foods/new.blade.php
@@ -20,29 +20,17 @@
     </div>
     <div class="food-wrap mx-auto m-1" style="width:50%">
       <div class="food-box col-md-6">
-        @if($errors->first('name'))
-        <div class="validation mx-auto m-1">{{ $errors->first('name') }}</div>
-        @endif
         <input type="text" class="form-control m-1" id="title-input" placeholder="食材" name="name2">
       </div>
       <div class="food-box col-md-6">
-        @if($errors->first('quantity'))
-        <div class="validation m-1">{{ $errors->first('quantity') }}</div>
-        @endif
         <input type="text" class="form-control m-1" placeholder="数量" name="quantity2">
       </div>
     </div>
     <div class="food-wrap mx-auto m-1" style="width:50%">
       <div class="food-box col-md-6">
-        @if($errors->first('name'))
-        <div class="validation mx-auto m-1">{{ $errors->first('name') }}</div>
-        @endif
         <input type="text" class="form-control m-1" id="title-input" placeholder="食材" name="name3">
       </div>
       <div class="food-box col-md-6">
-        @if($errors->first('quantity'))
-        <div class="validation m-1">{{ $errors->first('quantity') }}</div>
-        @endif
         <input type="text" class="form-control m-1" placeholder="数量" name="quantity3">
       </div>
     </div>
