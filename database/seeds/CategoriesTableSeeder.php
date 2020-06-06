438cf2b6f216018154345789e2e8f17e9d45a7cd
ekusuy26 <y.yuu6221@gmail.com>
2020-06-06 15:24:04 +0900

    Add others to seeder file

--- database/seeds/CategoriesTableSeeder.php
+++ database/seeds/CategoriesTableSeeder.php
@@ -11,7 +11,7 @@ class CategoriesTableSeeder extends Seeder
      */
     public function run()
     {
-        $foodCategories = array('穀物・いも類', 'まめ類', '野菜', '果実', 'きのこ', '海草', '魚', '海産', '肉', '卵・乳製品', 'お菓子', '飲み物', '調味料' );
+        $foodCategories = array('穀物・いも類', 'まめ類', '野菜', '果実', 'きのこ', '海草', '魚', '海産', '肉', '卵・乳製品', 'お菓子', '飲み物', '調味料', 'その他' );
         foreach ($foodCategories as $foodCategory){
             DB::table('categories')->insert([
                 'name' => $foodCategory,
