cf027bc30cfa68e8325a030a6cbbed30cc6a6483
ekusuy26 <y.yuu6221@gmail.com>
2020-06-07 16:18:09 +0900

    Also applies to input fields 2 and 3

--- resources/views/auth/foods/new.blade.php
+++ resources/views/auth/foods/new.blade.php
@@ -39,18 +39,52 @@
       </div>
     </div>
     <div class="food-wrap mx-auto m-1" style="width:50%">
+      <select class="food-box col-md-3" name="category2">
+        <option value="0">選択してください</option>
+        <option value="1">穀物・いも類</option>
+        <option value="2">まめ類</option>
+        <option value="3">野菜</option>
+        <option value="4">果実</option>
+        <option value="5">きのこ</option>
+        <option value="6">海草</option>
+        <option value="7">魚</option>
+        <option value="8">海産</option>
+        <option value="9">肉</option>
+        <option value="10">卵・乳製品</option>
+        <option value="11">お菓子</option>
+        <option value="12">飲み物</option>
+        <option value="13">調味料</option>
+        <option value="14">その他</option>
+      </select>
       <div class="food-box col-md-6">
         <input type="text" class="form-control m-1" id="title-input" placeholder="食材2" name="name2">
       </div>
-      <div class="food-box col-md-6">
+      <div class="food-box col-md-3">
         <input type="text" class="form-control m-1" placeholder="数量2" name="quantity2">
       </div>
     </div>
     <div class="food-wrap mx-auto m-1" style="width:50%">
+      <select class="food-box col-md-3" name="category3">
+        <option value="0">選択してください</option>
+        <option value="1">穀物・いも類</option>
+        <option value="2">まめ類</option>
+        <option value="3">野菜</option>
+        <option value="4">果実</option>
+        <option value="5">きのこ</option>
+        <option value="6">海草</option>
+        <option value="7">魚</option>
+        <option value="8">海産</option>
+        <option value="9">肉</option>
+        <option value="10">卵・乳製品</option>
+        <option value="11">お菓子</option>
+        <option value="12">飲み物</option>
+        <option value="13">調味料</option>
+        <option value="14">その他</option>
+      </select>
       <div class="food-box col-md-6">
         <input type="text" class="form-control m-1" id="title-input" placeholder="食材3" name="name3">
       </div>
-      <div class="food-box col-md-6">
+      <div class="food-box col-md-3">
         <input type="text" class="form-control m-1" placeholder="数量3" name="quantity3">
       </div>
     </div>
