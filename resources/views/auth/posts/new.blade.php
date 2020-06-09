c1dfec005f736203f8295e0c9c9d3e14f96cce8c
ekusuy26 <y.yuu6221@gmail.com>
2020-06-10 00:43:16 +0900

    Delete category field of post.new

--- resources/views/auth/posts/new.blade.php
+++ resources/views/auth/posts/new.blade.php
@@ -19,65 +19,14 @@
                     <textarea cols="60" class="form-control" id="body-input" placeholder="説明文" name="body"></textarea>
                     <div class="post-new-material">
                         <div class="post-new-material-box">
-                            <select class="food-box col-md-3" name="category1" style="height:36px;">
-                                <option value="initial">選択してください</option>
-                                <option value="1">穀物・いも類</option>
-                                <option value="2">まめ類</option>
-                                <option value="3">野菜</option>
-                                <option value="4">果実</option>
-                                <option value="5">きのこ</option>
-                                <option value="6">海草</option>
-                                <option value="7">魚</option>
-                                <option value="8">海産</option>
-                                <option value="9">肉</option>
-                                <option value="10">卵・乳製品</option>
-                                <option value="11">お菓子</option>
-                                <option value="12">飲み物</option>
-                                <option value="13">調味料</option>
-                                <option value="14">その他</option>
-                            </select>
                             <input type="text" class="form-control" placeholder="材料" name="name1">
                             <input type="text" class="form-control" placeholder="数量" name="quantity1">
                         </div>
                         <div class="post-new-material-box">
-                        <select class="food-box col-md-3" name="category3" style="height:36px;">
-                                <option value="initial">選択してください</option>
-                                <option value="1">穀物・いも類</option>
-                                <option value="2">まめ類</option>
-                                <option value="3">野菜</option>
-                                <option value="4">果実</option>
-                                <option value="5">きのこ</option>
-                                <option value="6">海草</option>
-                                <option value="7">魚</option>
-                                <option value="8">海産</option>
-                                <option value="9">肉</option>
-                                <option value="10">卵・乳製品</option>
-                                <option value="11">お菓子</option>
-                                <option value="12">飲み物</option>
-                                <option value="13">調味料</option>
-                                <option value="14">その他</option>
-                            </select>
                             <input type="text" class="form-control" placeholder="材料" name="name2">
                             <input type="text" class="form-control" placeholder="数量" name="quantity2">
                         </div>
                         <div class="post-new-material-box">
-                        <select class="food-box col-md-3" name="category3" style="height:36px;">
-                                <option value="initial">選択してください</option>
-                                <option value="1">穀物・いも類</option>
-                                <option value="2">まめ類</option>
-                                <option value="3">野菜</option>
-                                <option value="4">果実</option>
-                                <option value="5">きのこ</option>
-                                <option value="6">海草</option>
-                                <option value="7">魚</option>
-                                <option value="8">海産</option>
-                                <option value="9">肉</option>
-                                <option value="10">卵・乳製品</option>
-                                <option value="11">お菓子</option>
-                                <option value="12">飲み物</option>
-                                <option value="13">調味料</option>
-                                <option value="14">その他</option>
-                            </select>
                             <input type="text" class="form-control" placeholder="材料" name="name3">
                             <input type="text" class="form-control" placeholder="数量" name="quantity3">
                         </div>
