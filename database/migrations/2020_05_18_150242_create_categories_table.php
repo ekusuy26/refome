0e64dd48810c0dec43f4730b9206d826ad0ee0aa
ekusuy26 <y.yuu6221@gmail.com>
2020-06-10 11:15:23 +0900

    Describe the initial value of the category seeder file in the migration file

--- database/migrations/2020_05_18_150242_create_categories_table.php
+++ database/migrations/2020_05_18_150242_create_categories_table.php
@@ -17,6 +17,23 @@ class CreateCategoriesTable extends Migration
             $table->bigIncrements('id');
             $table->string('name');
         });
+
+        DB::table('categories')->insert([
+            ['name' => '穀物・いも類'],
+            ['name' => 'まめ類'],
+            ['name' => '野菜'],
+            ['name' => '果実'],
+            ['name' => 'きのこ'],
+            ['name' => '海草'],
+            ['name' => '魚'],
+            ['name' => '海産'],
+            ['name' => '肉'],
+            ['name' => '卵・乳製品'],
+            ['name' => 'お菓子'],
+            ['name' => '飲み物'],
+            ['name' => '調味料'],
+            ['name' => 'その他'],
+        ]);
     }
 
     /**
